f0e617c8912e72e7a9870cc4738e66fce5470d2d
DeathSea <szmsdavid_wang@163.com>
2016-06-02 22:44:29 +0800

    写完了整套的RSA算法，算是，写完了，溢出的问题解决不能，位数目前为16位，太大怕算太慢

--- RSA/Main.cpp
+++ RSA/Main.cpp
@@ -1,29 +1,27 @@
 #include "RSA.h"
+#include "BinCalc.h"
 #include <iostream>
+#include <stdlib.h>
 int main()
 {
-	bool TESTA[16] = {
-		true, false, true, false, true, false, false, false, 
-		true, true, false, true, true, false, true, true
-	};//43227
-	bool TESTB[16] = {
-		true, false, false, true, true, false, false, true, 
-		true, true, true, true, true, false, true, true
-	};//39419
-	//bool Result[16];//0b1 0100001011010110
-	//bool o = BinAdd(TESTA, 16, TESTB, 16, Result, 16);
-	//bool hnc;
-	//o = BinMinus(TESTA, 16, TESTB, 16, Result, 16, hnc);//Result = 0b 1110 1110 0000
-	//int oo;
-	//oo = BinCmp(TESTA, 16, TESTB, 16);
-	//oo = BinCmp(TESTB, 16, TESTA, 16);
-	//oo = BinCmp(TESTA, 16, TESTA, 16);
-	//BinLeftShift(TESTA, 16, 20, R, 32);
-	//BinMul(TESTA, 16, TESTB, 16, R, 32);
-	//bool First[7] = { true, true, false, false, true, false, false };
-	//bool Div[2] = { true, true };
-	//bool R[7];
-	//bool M[7];
-	//BinDivMod(First, 7, Div, 2, R, 7, M, 7);
+	std::cout << "On calc prime num ... " << std::endl;
+	createprime();
+	std::cout << "On calc key ... " << std::endl;
+	calcKey();
+	bool * testbool = new bool[7]{
+		true, false, false,
+			false, true, false, true
+	};
+	std::cout << "before encryption:" << std::endl;
+	outputboolarray(testbool,7);
+	std::cout << std::endl;
+	BinNum R = Encrypt(testbool, 7);
+	std::cout << "after encryption:" << std::endl;
+	outputB(R);
+	BinNum dec = Decrypt(R);
+	std::cout << "after decryption:" << std::endl;
+	outputB(dec);
+	std::cout << "if decryption is not equal the test ,may be overflow on calc (a+b) % n, i can't fix it,sorry for that" << std::endl;
+	system("pause");
 	return 0;
 }
--- RSA/RSA.cpp
+++ RSA/RSA.cpp
@@ -1,4 +1,6 @@
 #include "RSA.h"
+#include "BinPrime4096.h"
+#include <time.inl>
 int prime[305] = {
 	2, 3, 5, 7, 11,
 	13, 17, 19, 23, 29,
@@ -62,192 +64,13 @@ int prime[305] = {
 	1949, 1951, 1973, 1979, 1987,
 	1993, 1997, 1999, 2003, 2011
 };
-bool q[256], p[256];
-bool baseBinAdd(bool Ai, bool Bi, bool Ci, bool &Cip1)
-{
-	bool S = Ai ^ Bi ^ Ci;
-	Cip1 = (Ai & Bi) | (Bi & Ci) | (Ci & Ai);
-	return S;
-}
-bool BinAdd(bool * A, int Adigit, bool * B, int Bdigit, bool * result, int Rdigit)
-{
-	//if (Rdigit != max<int>(Adigit, Bdigit)){ return true; }
-	int maxLength = max<int>(Adigit, Bdigit);
-	int minLength = min<int>(Adigit, Bdigit);
-	bool *LongestOne, *ShortestOne;
-	if (maxLength == Adigit){ LongestOne = A; ShortestOne = B; }
-	else{ LongestOne = B; ShortestOne = A; }
-	bool c = false;
-	int lIndex = maxLength - 1;
-	int RIndex = Rdigit - 1;
-	for (int index = minLength - 1; index >= 0; index--,RIndex--,lIndex--)
-	{
-		result[RIndex] = baseBinAdd(ShortestOne[index], LongestOne[lIndex], c, c);
-	}
-	while (RIndex >= 0)
-	{
-		result[RIndex] = baseBinAdd(LongestOne[lIndex], 0, c, c);
-		lIndex--; RIndex--;
-	}
-	return c;
-}
-void BinComplement(bool *N,int Adigit)
-{
-	int index = Adigit - 1;
-	while (index >= 0)
-	{
-		N[index] = !N[index];
-		index--;
-	}
-	bool One[1] = { true };
-	BinAdd(N, Adigit, One, 1, N, Adigit);
-}
-bool BinMinus(bool * A, int Adigit, bool * B, int Bdigit, bool * result, int Rdigit,bool &hnc)
-{
-	int MaxLength = max<int>(Adigit, Bdigit);
-	int MinLength = min<int>(Adigit, Bdigit);
-	bool * ShortOne = MinLength == Bdigit ? B : A;
-	bool * LongestOne = MaxLength == Adigit ? A : B;
-	bool * Tmp = (bool *)malloc(sizeof(bool)*MaxLength);
-	memset(Tmp, false, MaxLength);
-	int sIndex = MinLength - 1;
-	int cIndex = MaxLength - 1;
-	while (sIndex >= 0)
-	{
-		Tmp[cIndex--] = ShortOne[sIndex--];
-	}
-	bool sign;
-	if (MaxLength == Adigit)
-	{
-		BinComplement(Tmp, MaxLength);
-		bool c = BinAdd(A, Adigit, Tmp, MaxLength, result, Rdigit);
-		sign = baseBinAdd(false, true, c, hnc);
-		free(Tmp);
-		return sign;
-	}
-	else
-	{
-		bool * D = (bool *)malloc(sizeof(bool)*MaxLength);
-		memcpy_s(D, MaxLength, B, MaxLength);
-		BinComplement(D, Bdigit);
-		bool c = BinAdd(Tmp, MaxLength, D, Bdigit, result, Rdigit);
-		sign = baseBinAdd(false, true, c, hnc);
-		free(D);
-		free(Tmp);
-		return sign;
-	}
-
-}
-int BinCmp(bool *A, int Adigit, bool *B, int Bdigit)
-{
-	//wrong
-	int Afirst1 = 0,Bfirst1 = 0;
-	int Aindex = 0, Bindex = 0;
-	bool tmp = false;
-	for (; Aindex < Adigit; Aindex++)
-	{
-		tmp |= A[Aindex];
-	}
-	if (!tmp)return -1;
-	tmp = false;
-	for (; Bindex < Bdigit; Bindex++)
-	{
-		tmp |= B[Bindex];
-	}
-	if (!tmp)return 1;
-	for (Aindex = Afirst1; Aindex < Adigit; Aindex++)
-	{
-		if (A[Aindex] == true){ Afirst1 = Aindex; break; }
-	}
-	for (Bindex = Bfirst1; Bindex < Bdigit; Bindex++)
-	{
-		if (B[Bindex] == true){ Bfirst1 = Bindex; break; }
-	}
-
-	if ((Adigit - Afirst1) < (Bdigit - Bfirst1)) return -1;
-	else if ((Adigit - Afirst1) > (Bdigit - Bfirst1))return 1;
-	else if ((Afirst1 == Adigit - 1) && (Bfirst1 == Bdigit - 1))return 0;
-
-	for (Bindex = Bfirst1; Bindex < Bdigit; Bindex++,Afirst1++)
-	{
-		if (B[Bindex] && !A[Afirst1])return -1;
-		else if (!B[Bindex] && A[Afirst1])return 1;
-	}
-	return 0;
-}
-void BinLeftShift(bool * A,int Adigit,int Shift,bool * Result,int Rdigit)
-{
-	if (Rdigit < Adigit){ return; }
-	int index = Adigit - 1,j;
-	memset(Result, false, Rdigit);
-	for (j = Rdigit - Shift - 1;j >= 0 && j > Rdigit - Shift - Adigit - 1; j--,index --)
-	{
-		Result[j] = A[index];
-	}
-}
-bool BinMul(bool * A, int Adigit, bool * B, int Bdigit, bool * Result, int Rdigit)
-{
-	if (Rdigit < Adigit + Bdigit - 1)return true;
-	bool * Tmp = (bool *)malloc(sizeof(bool)*Rdigit);
-	bool * TT = (bool *)malloc(sizeof(bool)*Rdigit);
-	memset(Result, false, Rdigit);
-	memset(Tmp, false, Rdigit);
-	int Bindex = Bdigit - 1,Tindex = Rdigit - 1,Aindex = Adigit - 1;
-	for (; Aindex >= 0; )
-	{
-		Tmp[Tindex--] = A[Aindex--];
-	}
-	bool C;
-	while (Bindex >= 0)
-	{
-		if (B[Bindex--] == true)
-		{
-			C = BinAdd(Tmp, Rdigit, Result, Rdigit, Result, Rdigit);
-		}
-		BinLeftShift(Tmp, Rdigit, 1, TT, Rdigit);
-		memcpy_s(Tmp,Rdigit,TT,Rdigit);
-	}
-	free(Tmp); free(TT);
-	return C;
-}
-//http://www.cnblogs.com/AndyJee/p/4575152.html
-bool BinDivMod(bool *A, int Adigit, bool *B, int Bdigit, 
-	bool * DivResult, int Ddigit,
-	bool * ModResult,int Mdigit)
-{
-	if (BinCmp(A, Adigit, B, Bdigit) == -1)
-	{
-		memset(DivResult, false, Ddigit);
-		int offset = Adigit - Mdigit > 0?Adigit - Mdigit:0;
-		memmove_s(ModResult,Mdigit,A+offset,Mdigit);
-		return false;
-	}
-	bool *x = (bool *)malloc(sizeof(bool)*(Adigit + 1));
-	memset(x, false, Adigit + 1);
-	memmove_s(x + (Adigit + 1 - Bdigit),Adigit + 1,B,Bdigit);
-	bool *lsx = (bool *)malloc(sizeof(bool)*(Adigit + 1));
-	bool *y = (bool *)malloc(sizeof(bool)*(Adigit + 1));
-	memset(y, false, Adigit + 1);
-	y[Adigit] = true;
-	BinLeftShift(x, Adigit + 1, 1, lsx, Adigit + 1);
-	while (BinCmp(A, Adigit, lsx, Adigit + 1) != -1)
-	{
-		memcpy_s(x, Adigit + 1, lsx, Adigit + 1);
-		//BinLeftShift(x, Adigit + 1, 1, lsx, Adigit + 1);
-		BinLeftShift(y, Adigit + 1, 1, lsx, Adigit + 1);
-		memcpy_s(y, Adigit + 1, lsx, Adigit + 1);
-		BinLeftShift(x, Adigit + 1, 1, lsx, Adigit + 1);
-	}
-	bool hnc;
-	BinMinus(A, Adigit, x, Adigit + 1, lsx, Adigit + 1, hnc);
-	BinDivMod(lsx, Adigit + 1, B, Bdigit, DivResult, Ddigit, ModResult, Mdigit);
-	return BinAdd(DivResult,Ddigit,y,Adigit+1,DivResult,Ddigit);
-}
 
 int ExGCD(int a, int b, int * x, int *y)
 {
 	if (b == 0)
 	{
+		*x = 1;
+		*y = 0;
 		return a;
 	}
 	else
@@ -255,7 +78,7 @@ int ExGCD(int a, int b, int * x, int *y)
 		int r = ExGCD(b, a%b, x, y);
 		int t = *x;
 		*x = *y;
-		*y = (t - a / b) * *y;
+		*y = t - (a / b) * *y;
 		return r;
 	}
 }
@@ -271,4 +94,141 @@ int mod(int a, int b, int n)
 		b >>= 1;
 	}
 	return result;
+}
+int randint(int start, int end)
+{
+	static bool first = false;
+	if (!first){ 
+		srand((unsigned int)time(NULL));
+		first = true;
+	}
+	int thisint = rand() % (end - start + 1) + start;
+	return thisint;
+}
+void outputboolarray(bool *N, int length)
+{
+	int index = 0;
+	while (index < length)
+	{
+		if (N[index]){ std::cout << "1"; }
+		else{ std::cout << "0"; }
+		index++;
+		if (index % 4 == 0){ std::cout << " "; }
+	}
+}
+void outputB(BinNum &A)
+{
+	if (!A.HighisZero){ 
+		outputboolarray(A.High, MAXDIGIT); 
+		std::cout << " "; 
+	}
+	outputboolarray(A.Low, MAXDIGIT); 
+	std::cout << std::endl;
+}
+extern BinNum p = BinNum();
+extern BinNum q = BinNum();
+bool one[1] = { true };
+extern BinNum publicKeyN = BinNum();
+extern BinNum publicKeyE = BinNum();
+extern BinNum privateKeyD = BinNum();
+extern BinNum One = BinNum(one, 1);
+
+void createprime()
+{
+	p.Low[MAXDIGIT - 1] = true;
+	p.Low[0] = true;
+	q.Low[MAXDIGIT - 1] = true;
+	q.Low[0] = true;
+	int changebit, times, bit;
+	while (1)
+	{
+		changebit = randint(1, MAXDIGIT - 2);
+		for (times = 0; times < changebit; times++)
+		{
+			bit = randint(1, MAXDIGIT - 2);
+			if (!p.Low[bit]){ p.Low[bit] = true; }
+			else{ times--; }
+		}
+		if (isPrime(p))break;
+		else
+		{
+			memset(p.Low, false, MAXDIGIT);
+			p.Low[MAXDIGIT - 1] = true;
+			p.Low[0] = true;
+		}
+	}
+	while (1)
+	{
+		changebit = randint(1, MAXDIGIT - 2);
+		for (times = 0; times < changebit; times++)
+		{
+			bit = randint(1, MAXDIGIT - 2);
+			if (!q.Low[bit]){ q.Low[bit] = true; }
+			else{ times--; }
+		}
+		if (isPrime(q))break;
+		else
+		{
+			memset(q.Low, false, MAXDIGIT);
+			q.Low[MAXDIGIT - 1] = true;
+			q.Low[0] = true;
+		}
+	}
+	////p = 0b1011 1001 1110 1111
+	//bool * Q = new bool[16]{
+	//true,false,true,true ,
+	//	true,false,false,true,
+	//	true,true,true,false,
+	//	true,true,true,true
+	//};
+	////q = 0b1111 1111 1110 1111
+	//bool * P = new bool[16]{
+	//true,true,true,true,
+	//	true,true,true,true,
+	//	true,true,true,false,
+	//	true,true,true,true
+	//};
+	//q = BinNum(Q, 16);
+	//p = BinNum(P, 16);
+	std::cout << "the prime num p is: " << std::endl;
+	outputB(p);
+	std::cout << "the prime num q is: " << std::endl;
+	outputB(q);
+}
+void calcKey()
+{
+	publicKeyN = p * q;
+	BinNum omegaN = (p - One)*(q - One);
+	BinNum Tmp = BinNum();
+	BinNum x = BinNum();
+	BinNum y = BinNum();
+	for (int Pindex = 0; Pindex < 564;Pindex++)
+	{
+		Tmp = BinNum(allBinPrime[Pindex],12);
+		BinNum result = ExGCD(Tmp, omegaN, x, y);
+		if (result == One)
+		{
+			publicKeyE = BinNum(Tmp);
+			while (x.sign == true){ x = x + omegaN; }
+			privateKeyD = BinNum(x);
+			break;
+		}
+	}
+	std::cout << "the private key D is: " << std::endl;
+	outputB(privateKeyD);
+	std::cout << "the public key E is: " << std::endl;
+	outputB(publicKeyE);
+	std::cout << "the public key N is: " << std::endl;
+	outputB(publicKeyN);
+	std::cout << "the omega N is: " << std::endl;
+	outputB(omegaN);
+}
+BinNum Encrypt(bool * M,int length)
+{
+	BinNum m = BinNum(M, length);
+	return ModNsquare(m, publicKeyE, publicKeyN);
+}
+BinNum Decrypt(BinNum c)
+{
+	return ModNsquare(c, privateKeyD, publicKeyN);
 }
